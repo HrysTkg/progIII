4092f2cd1322b39ad1468141df578a8240e8ea4b
HrysTkg <44859000+HrysTkg@users.noreply.github.com>
2018-11-27 00:39:16 +0900

    Update tetris.h

--- tetris.h
+++ tetris.h
@@ -1,8 +1,9 @@
-#define WIDTH 20
-#define HEIGHT 20
-#define LWALL (COLS/2 - WIDTH)
-#define RWALL (LWALL + WIDTH)
+#define WIDTH 20		//ステージの幅
+#define HEIGHT 20		//ステージの高さ
+#define LWALL (COLS/2 - WIDTH)	//ステージ左壁のx座標
+#define RWALL (LWALL + WIDTH)	//ステージ右壁のx座標
 
+//ブロック構成要素の差分座標を定義
 #define TYPE1 {{-2,0},{-1.0},{0,0},{1,0}}
 #define TYPE2 {{1,0},{0,0},{-1,1},{0,1}}
 #define TYPE3 {{-1,0},{0,0},{1,0},{-1,1}}
@@ -11,49 +12,49 @@
 #define TYPE6 {{-1,1},{0,1},{0,0},{1,0}}
 #define TYPE7 {{-1,0},{0,0},{0,1},{1,1}}
 
-//Escキー
-#define ESC 0x1b
+#define ESC 0x1b		//Escキーの値
 
-#define WAIT_TIME 200
-#define RECORD_FILE "record.txt"
+#define WAIT_TIME 200		//待ち時間の定義
+#define RECORD_FILE "record.txt"//記録保存ファイルの定義
 
+/* 交換関数のマクロ定義 */
 #define swap(type,a,b) {type t=a;a=b;b=t;}
 
-int stacked[WIDTH][HEIGHT];
-int score;
+int stacked[WIDTH][HEIGHT];	//積まれたブロックを管理する
+int score;			//スコア
 
+//座標構造体
 typedef struct{
 	int x;
 	int y;
 }Point;
 
+//ブロック構造体
 typedef struct{
-	Point bp[4];	//構成ブロック群の座標
+	Point bp[4];	//構成ブロック要素の差分座標
 	Point pos;	//中心座標
-	int life;
 }Block;
 
+//記録を保存する構造体
 typedef struct {
 	char name[64];
 	int score;
 }Record;
 
-void printStage();	//ステージを描画する関数
+/* 以下関数宣言 */
+void printStage();
 void run();
-void printBlock(Block b);	//ブロックを描画する関数
-void clearBlock(Block b);	//ブロックを消す関数
-void moveBlock(Block* b, int key);	//キー操作もしくは時間経過によりブロックを移動する関数
-void rotateBlock(Block* b);	//ブロックを右回りに90度回転させる関数
-void nextBlock(Block* mov, Block* nxt, Point types[7][4]);	//次のブロックを決定する関数
-int isEnableBlock(Block b);	//ブロックが有効か判断する関数
-
-void printStackedBlock();	//積まれたブロックを描画する関数
+void printBlock(Block b);
+void moveBlock(Block* b, int key);
+void rotateBlock(Block* b);
+void nextBlock(Block* mov, Block* nxt, Point types[7][4]);
+int isEnableBlock(Block b);
+void printStackedBlock();
 void clearStackedBlock();
-void judge();	//揃ったブロック行がないか判定する関数
-void addStackedBlock(Block b);	//ブロックを積む関数
-void eraceColumn(int e[HEIGHT], int c);	//揃ったブロック行を消す
-void organizeStacked(int o);	//積まれたブロックの整理
-int isGameOver();	//ゲームオーバー判定
-
+void judge();
+void addStackedBlock(Block b);
+void eraceColumn(int e[HEIGHT], int c);
+void organizeStacked(int o);
+int isGameOver();
 void showRanking();
 void saveScore();
