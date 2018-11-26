5db5c9bd51276f5104e6f40c50fca21502078809
HrysTkg <44859000+HrysTkg@users.noreply.github.com>
2018-11-27 00:32:58 +0900

    Update tetris.c
    コメントいっぱい

--- tetris.c
+++ tetris.c
@@ -5,53 +5,29 @@
 #include <stdlib.h>
 #include "tetris.h"
 
-void printStage(){
-	int i;
-	int j = 0;
-	for(i = LWALL; i <= RWALL; i++){
-		mvaddch(LINES / 2 - HEIGHT / 2 - 1, i, (j % 2 == 0)?'-':'=');
-		mvaddch(LINES / 2 + HEIGHT / 2, i, (j++ % 2 == 0)?'-':'=');
-	}
-	for(i = LINES / 2 - HEIGHT / 2; i < LINES / 2 + HEIGHT / 2; i++){
-   	mvaddch(i, LWALL, '|');
-    mvaddch(i, RWALL, '|');
-	}
-	mvprintw(LINES / 2 - 2, RWALL + 5,"Next Block");
-	mvprintw(LINES / 2 - 1, RWALL + 5,"|        |");
-	mvprintw(LINES / 2    , RWALL + 5,"|        |");
-	mvprintw(LINES / 2 + 1, RWALL + 5,"|        |");
-	mvprintw(LINES / 2 + 2, RWALL + 5,"|        |");
-	mvprintw(LINES / 2 + 3, RWALL + 5,"-=-=-=-=-=");
-	mvprintw(LINES / 2 + 4, RWALL + 5, "Score:%d",score);
-}
-
-void printTitle(){
-	mvprintw(LINES/2-3,COLS/2 - 7,"WELCOME TO TETRIS");
-	mvprintw(LINES/2  ,COLS/2 - 5,"[s] : Start");
-	mvprintw(LINES/2+2,COLS/2 - 5,"[r] : Ranking");
-	mvprintw(LINES/2+4,COLS/2 - 5,"[Esc] : Exit");
-}
-
+/* ゲーム実行関数 */
 void run(){
-	int key;	//キー入力受け取り用の変数
-
-	Block moving, next;	//動いているブロックと次のブロック
-	int blockInitX = LWALL + WIDTH / 2;
-	int blockInitY = LINES / 2 - HEIGHT / 2;
+	int key;				//キー入力受け取り用の変数
+	Block moving, next;			//移動ブロックと次ブロック
+	int blockInitX = LWALL + WIDTH / 2;	//落下ブロックの初期x座標
+	int blockInitY = LINES / 2 - HEIGHT / 2;//落下ブロックの初期y座標
+	//次のブロックは画面上固定なので、位置をここで指定しておく
 	next.pos.x = RWALL + 10;
 	next.pos.y = LINES / 2;
 
+	//ブロックの種類を設定するための配列
 	Point types[7][4] = {TYPE1,TYPE2,TYPE3,TYPE4,TYPE5,TYPE6,TYPE7};
-	int i,j,k;
+	int i,j,k;				//ループ変数
 
-	timeout(WAIT_TIME);
-	srand((unsigned int)time(NULL));
-	k = rand() % 7;
+	timeout(WAIT_TIME);			//キー入力のタイムアウトをWAIT_TIMEミリ秒に設定する
+	srand((unsigned int)time(NULL));	//乱数のシードに現在の時刻を使用
 
+//ゲームスタート画面
 GAME_START:
 	printTitle();
 	for(;;){
 		key = getch();
+		//キーがs→ゲームスタート,r→ランキング表示,Escキー→終了
 		if(key == 's'){
 			clear();
 			goto GAME_INIT;
@@ -62,45 +38,56 @@ GAME_START:
 			return;
 		}
 	}
+//ゲームの初期化 スコアをリセットし,積まれたブロックを全て消す
 GAME_INIT:
 	score = 0;
 	for(i = 0; i < WIDTH; i++)
 		for(j = 0; j < HEIGHT; j++)
 			stacked[i][j] = 0;
+//ゲーム本体部
 GAME:
+	//移動ブロックと次ブロックを決定するため,2回nextBlockを呼び出す
 	nextBlock(&moving, &next, types);
 	nextBlock(&moving, &next, types);
-	//メインループ
+	//メインループ(Escキーで抜ける)
 	while((key = getch()) != ESC){
 		clear();
 		printStage();
-		Block cp = moving;
-		cp.pos.y += 1;
+		
+		Block cp = moving;	//移動ブロックのコピーを作成する
+		cp.pos.y += 1;		//コピーを1つ下へ動かしてみる
+		//コピーが最下地点または他ブロックに衝突しているか調べる
 		if(isEnableBlock(cp) == 0){
-			addStackedBlock(moving);
+			addStackedBlock(moving);	//コピーが衝突していた場合 移動ブロックはそこで停止し,積む
 			judge();
 			if(isGameOver())
-				goto GAME_OVER;
-			nextBlock(&moving, &next, types);
-			score += 10;
+				goto GAME_OVER;		//ゲームオーバーなら終了画面へ遷移
+			nextBlock(&moving, &next, types);//次のブロックを生成
+			score += 10;			//ブロックが1つつまれると10点加算
 		} else {
-			moveBlock(&moving, key);
+			moveBlock(&moving, key);	//コピーが衝突していなかったら移動ブロックを動かす
 		}
+		//以下各描画処理
 		printStackedBlock();
 		printBlock(moving);
 		printBlock(next);
 	}
-	if(key == ESC) goto END;
+	if(key == ESC) goto END;	//Escキーが入力されていたなら終了
+//ゲームオーバー部
 GAME_OVER:
+	//積まれたブロックを点滅させる
 	for(i = 0; i < 11; i++){
 		(i % 2 == 0)?clearStackedBlock():printStackedBlock();
 		refresh();
 		usleep(WAIT_TIME * 1000);
 	}
+	
+	//以下ゲームオーバーのメッセージを表示
 	clear();
 	mvprintw(LINES/2    , COLS/2 - 5,"GAME OVER");
 	mvprintw(LINES/2 + 2, COLS/2 - 15,"Do you want to join Ranking? score:%d",score);
 	mvprintw(LINES/2 + 4, COLS/2 - 9,"[y] : Yes , [n] : No");
+	//キー入力ループ yキー→スコアを保存 nキー→スコアを保存しない Escキー→終了
 	while((key = getch()) != ESC){
 		if(key == 'y'){
 			saveScore();
@@ -112,81 +99,119 @@ GAME_OVER:
 			goto END;
 	}
 	clear();
-	goto GAME_START;
+	goto GAME_START;	//再びゲームスタート画面へ戻る
+//ゲーム終了部
 END:
 	return;
 }
 
+/* ステージ描画関数 */
+void printStage(){
+	int i;
+	int j = 0;
+	//ステージの床を天井を-=-=で描画
+	for(i = LWALL; i <= RWALL; i++){
+		mvaddch(LINES / 2 - HEIGHT / 2 - 1, i, (j % 2 == 0)?'-':'=');
+		mvaddch(LINES / 2 + HEIGHT / 2, i, (j++ % 2 == 0)?'-':'=');
+	}
+	//ステージの左右壁と|で描画
+	for(i = LINES / 2 - HEIGHT / 2; i < LINES / 2 + HEIGHT / 2; i++){
+   		mvaddch(i, LWALL, '|');
+		mvaddch(i, RWALL, '|');
+	}
+	//次のブロックの表示区画とスコアを表示
+	mvprintw(LINES / 2 - 2, RWALL + 5,"Next Block");
+	mvprintw(LINES / 2 - 1, RWALL + 5,"|        |");
+	mvprintw(LINES / 2    , RWALL + 5,"|        |");
+	mvprintw(LINES / 2 + 1, RWALL + 5,"|        |");
+	mvprintw(LINES / 2 + 2, RWALL + 5,"|        |");
+	mvprintw(LINES / 2 + 3, RWALL + 5,"-=-=-=-=-=");
+	mvprintw(LINES / 2 + 4, RWALL + 5, "Score:%d",score);
+}
+
+/* スタート画面のタイトルを表示する関数 */
+void printTitle(){
+	mvprintw(LINES/2-3,COLS/2 - 7,"WELCOME TO TETRIS");
+	mvprintw(LINES/2  ,COLS/2 - 5,"[s] : Start");
+	mvprintw(LINES/2+2,COLS/2 - 5,"[r] : Ranking");
+	mvprintw(LINES/2+4,COLS/2 - 5,"[Esc] : Exit");
+}
+
+/* 引数のブロックbを@で表示する関数 */
 void printBlock(Block b){
 	int i;
 	for(i = 0; i < 4; i++){
+		//ブロックの中心座標posとブロックを構成する各要素の差分bpの和で@を表示させる位置を計算できる
 		mvaddch(b.pos.y + b.bp[i].y, b.pos.x + b.bp[i].x, '@');
 	}
 }
 
-void clearBlock(Block b){
-	int i;
-	for(i = 0; i < 4; i++){
-		mvaddch(b.pos.y + b.bp[i].y, b.pos.x + b.bp[i].x, ' ');
-	}
-}
-
+/* 引数のブロックbをkeyの値によって移動させる関数 */
 void moveBlock(Block* b, int key){
+	//bのコピーを作成し,コピーに対して処理を施す
 	Block cp = *b;
 	switch(key){
-	case KEY_LEFT:
+	case KEY_LEFT:			//左へ移動
 		cp.pos.x -= 1;
 		break;
-	case KEY_RIGHT:
+	case KEY_RIGHT:			//右へ移動
 		cp.pos.x += 1;
 		break;
-	case KEY_UP:
+	case KEY_UP:			//回転
 		rotateBlock(&cp);
 		break;
-	default:
+	default:			//キー操作が無かったら単純落下
 		cp.pos.y += 1;
 	}
+	//コピーが有効(他の要素と干渉していない)か確認し,有効ならbをコピーで更新
 	if(isEnableBlock(cp) == 1){
 		*b = cp;
 	}
 }
 
+/* 引数のブロックbを回転させる関数 */
 void rotateBlock(Block* b){
 	int i;
+	//回転は回転行列に基づく 一般的な回転行列にθ=-π/2を代入すれば明らか
 	for(i = 0; i < 4; i++){
-		int tempX = b->bp[i].x;
-		int tempY = b->bp[i].y;
-		b->bp[i].x = tempY;
-		b->bp[i].y = -1 * tempX;
+		int temp = b->bp[i].x;
+		b->bp[i].x = b->bp[i].y;
+		b->bp[i].y = -1 * temp;
 	}
 }
 
+/* 次ブロックを更新し移動ブロックを前の次ブロックで更新する関数 */
 void nextBlock(Block* mov, Block* nxt, Point types[7][4]){
 	int i,k;
-	k = rand() % 7;
-	//k = 0;
-	mov->pos.x = LWALL + WIDTH / 2;
+	k = rand() % 7;				//次ブロックを乱数で決定する
+	mov->pos.x = LWALL + WIDTH / 2;		//移動ブロックの中心座標を初期化
         mov->pos.y = LINES / 2 - HEIGHT / 2;
 	for(i = 0; i < 4; i++){
-		mov->bp[i] = nxt->bp[i];
-		nxt->bp[i] = types[k][i];
+		mov->bp[i] = nxt->bp[i];	//移動ブロックを次ブロックで更新
+		nxt->bp[i] = types[k][i];	//次ブロックの要素差分を更新
 	}
 }
 
+/* 引数のブロックbが有効か調べる関数 */
 int isEnableBlock(Block b){
-	int btm = LINES / 2 + HEIGHT / 2;
-  int i;
-  for(i = 0; i < 4; i++){
-		int bX = b.pos.x + b.bp[i].x;
+	int btm = LINES / 2 + HEIGHT / 2;	//ステージ床のy座標を取得
+	int i;
+	//ブロックの各要素について調べる
+  	for(i = 0; i < 4; i++){
+		int bX = b.pos.x + b.bp[i].x;	//ブロック要素の座標を取得
 		int bY = b.pos.y + b.bp[i].y;
-    if(bY == btm || bX == LWALL || bX == RWALL || stacked[bX - LWALL][bY - (LINES - HEIGHT) / 2] == 1)
-      return 0;
-  }
-  return 1;
+		//取得した座標が床・壁・積まれたブロックに重なっていないか調べる
+    		if(bY == btm || bX == LWALL || bX == RWALL || stacked[bX - LWALL][bY - (LINES - HEIGHT) / 2] == 1)
+      			return 0;		//1つでも重なっていれば0を返却
+  	}
+  	return 1;	//有効であるので1を返却
 }
 
+/* 積まれたブロックを描画する */
 void printStackedBlock(){
 	int i,j;
+	//stacked[x][y]が1ならば,ステージ上の(x,y)地点にはブロックが積まれている
+	//ここで,(x,y)はステージ上での座標であるため、実際にブロックを表示するスクリーン座標は別で計算する必要あり
 	for(i = 0; i < WIDTH; i++){
 		for(j = 0; j < HEIGHT; j++){
 			if(stacked[i][j] != 0)
@@ -195,8 +220,10 @@ void printStackedBlock(){
 	}
 }
 
+/* 積まれたブロックの描画を消す関数 */
 void clearStackedBlock(){
 	int i,j;
+	//printStackedBlockの表示部分を空白' 'に変更しただけ
 	for(i = 0; i < WIDTH; i++){
 		for(j = 0; j < HEIGHT; j++){
 			if(stacked[i][j] != 0)
@@ -205,35 +232,40 @@ void clearStackedBlock(){
 	}
 }
 
+/* 揃った行がないかを判定する */
 void judge(){
-	int i, j, cnt = 0;
-	int completedColumn[HEIGHT] = {};
+	int i, j, cnt = 0;			//cntは揃った行をカウントする変数
+	int completedColumn[HEIGHT] = {};	//揃った行にフラグを立てるための配列
 	for(j = 0; j < HEIGHT; j++){
 		i = 0;
-		//printf("%d ",s[i][j]);
-		while (stacked[i+1][j] != 0) {
-			if(i == WIDTH - 2){
-				completedColumn[j] = 1;
-				cnt++;
-				break;
+		//行を走査し,stackedの行全てが1であるところを探す
+		while (stacked[i + 1][j] != 0) {
+			if(i == WIDTH - 2){		//行の終点へ到達したなら
+				completedColumn[j] = 1;	//対応する行へフラグを立てて
+				cnt++;			//カウントし
+				break;			//ブレイクして次の行へ
 			}
 			i++;
 		}
 	}
+	//揃った行が1つでもあれば,eraceColumnでその行を消去する
 	if(cnt != 0)
 		eraceColumn(completedColumn, cnt);
 }
 
+/* 揃った行を消去する関数 引数eはそろった行のフラグ,cは揃った行の数 */
 void eraceColumn(int e[HEIGHT], int c){
 	int i, j, k;
-	int *completedColumn = (int *)malloc(sizeof(int) * c);
+	int *completedColumn = (int *)malloc(sizeof(int) * c);	//揃った行のy座標を格納する配列をmallocで確保
 	j = 0;
 	for(i = 0; i < HEIGHT; i++){
+		//フラグが立っている行の添え字を保存 個の値がy座標と対応する
 		if(e[i] == 1){
 			completedColumn[j++] = i;
 		}
 	}
 	printStackedBlock();
+	//消去する行を6回点滅させる
 	for(i = 0; i < 7; i++){
 		for(j = 0; j < c; j++){
 			for(k = LWALL + 1; k < RWALL; k++){
@@ -243,14 +275,17 @@ void eraceColumn(int e[HEIGHT], int c){
 		refresh();
 		usleep(WAIT_TIME * 1000);
 	}
-
+	//stackedの値を消去後のものに変化させる
 	for(i = 0; i < c; i++){
 		organizeStacked(completedColumn[i]);
 	}
-	free(completedColumn);
+	free(completedColumn);		//確保した配列を解放
 }
 
+/* 引数で与えられた行を消去し,ブロックの並びを整える関数 */
 void organizeStacked(int cc){
+	/* 処理は再帰で行う. 引数が0の場合,処理が完了. 
+	それ以外は,引数の示す行をその一つ上の行で上書きし,cc - 1を新たに引数として再帰 */
 	if(cc == 0){
 		score += 100;
 		return;
@@ -262,26 +297,31 @@ void organizeStacked(int cc){
 	organizeStacked(cc - 1);
 }
 
+/* 引数で与えられたブロックを積む関数(つまりstackedにbを追加する) */
 void addStackedBlock(Block b){
 	int i;
+	//bはスクリーン座標,stacked[x][y]の(x,y)はステージ座標であることに注意
 	for(i = 0; i < 4; i++){
 		stacked[b.pos.x + b.bp[i].x - LWALL][b.pos.y + b.bp[i].y - (LINES - HEIGHT) / 2] = 1;
 	}
 }
 
+/* ゲームオーバーかの判定を行う関数 */
 int isGameOver(){
+	//stackedにステージ天井まで到達したブロックがないかを調べる
 	int top = 0;
 	int i;
 	for(i = 0; i < WIDTH; i++){
 		if(stacked[i][top] == 1)
-			return 1;
+			return 1;	//1つでも該当するブロックがあれば1を返却
 	}
-	return 0;
+	return 0;	//無ければ0を返却
 }
 
+/* ランキングをファイルから取得し表示する関数 */
 void showRanking(){
-	FILE *fp = fopen(RECORD_FILE,"r");
-	Record rcd[10];
+	FILE *fp = fopen(RECORD_FILE,"r");	//ファイルをオープン ファイル名はtetris.hでマクロ定義
+	Record rcd[10];				//ランキングは10位まで表示させるため,要素数は10とする
 	int i,j;
 	i = 0;
 	while(fscanf(fp,"%s %d",rcd[i].name, &rcd[i].score) != EOF){
@@ -306,30 +346,32 @@ void showRanking(){
 	fclose(fp);
 }
 
+/* 現在のスコアをファイルに保存する関数 */
 void saveScore(){
-	FILE *fp = fopen(RECORD_FILE, "a");
+	FILE *fp = fopen(RECORD_FILE, "a");	//ファイルをオープン ファイル名はtetris.hでマクロ定義
 	clear();
+	//ランキング登録名を入力するための処理
 	mvprintw(LINES/2,(COLS-10)/2,"Enter your name");
-	move(LINES/2 + 2,(COLS-10)/2);
-	refresh();
-	echo();
-	char name[64];
-	timeout(-1);
-	getstr(name);
-	fprintf(fp, "%s %d\n",name, score);
-	noecho();
-	clear();
-	timeout(WAIT_TIME);
-	fclose(fp);
+	move(LINES/2 + 2,(COLS-10)/2);		//カーソルを適当な位置に移動させる
+	refresh();				//画面のリフレッシュを行い,画面表示を行う
+	echo();					//入力キーを表示させる設定に変更
+	char name[64];				//登録名を受け取る変数
+	timeout(-1);				//入力のタイムアウトを無くす
+	getstr(name);				//入力された情報を受け取る
+	fprintf(fp, "%s %d\n",name, score);	//ファイルに登録名とスコアを書式に従って書き込む
+	noecho();				//入力キーを表示させない設定に戻す
+	clear();				//画面クリア
+	timeout(WAIT_TIME);			//タイムアウトを再設定
+	fclose(fp);				//ファイルクローズ
 }
 
 int main(void){
-	initscr();
-	noecho();
-	curs_set(0);
-	cbreak();
-	keypad(stdscr,TRUE);
-	run();
-	endwin();
+	initscr();				//cursesを初期化
+	noecho();				//入力キーを非表示に設定
+	curs_set(0);				//カーソルを非表示に設定
+	cbreak();				//入力キーを即時利用に設定
+	keypad(stdscr,TRUE);			//キーパッドを設定
+	run();					//ゲーム実行
+	endwin();				//curses終了処理
 	return 0;
 }
